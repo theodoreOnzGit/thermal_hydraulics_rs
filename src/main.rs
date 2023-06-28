fff941786b258c35f209016223bc987a3e99ffc8
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2023-06-28 10:54:33 -0700

    v0.03 added fluid mechanics tests and main, all tested okay

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,102 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+// Note: //! indicates crate level documentation
+//
+//
+//! A Library which contains useful traits and methods for thermal 
+//! hydraulics calculations.
+//!
+//!
+//! This crate has heavy reliance on units of measure (uom) released under 
+//! Apache 2.0 license. So you'll need to get used to unit safe calculations
+//! with uom as well.
+//!
+//!
+//! This library was initially developed for 
+//! use in my PhD thesis under supervision 
+//! of Professor Per F. Peterson. It a thermal hydraulics
+//! library in Rust that is released under the GNU General Public License
+//! v 3.0. This is partly due to the fact that some of the libraries 
+//! inherit from GeN-Foam and OpenFOAM, both licensed under GNU General
+//! Public License v3.0.
+//!
+//! As such, the entire library is released under GNU GPL v3.0. It is a strong 
+//! copyleft license which means you cannot use it in proprietary software.
+//!
+//!
+//! License
+//!    This is a thermal hydraulics library written 
+//!    in rust meant to help with the
+//!    fluid mechanics and heat transfer aspects of the calculations
+//!    for the Compact Integral Effects Tests (CIET) and hopefully 
+//!    Gen IV Reactors such as the Fluoride Salt cooled High Temperature 
+//!    Reactor (FHR)
+//!     
+//!    Copyright (C) 2022-2023  Theodore Kay Chen Ong, Singapore Nuclear
+//!    Research and Safety Initiative, Per F. Peterson, University of 
+//!    California, Berkeley Thermal Hydraulics Laboratory
+//!
+//!    thermal_hydrualics_rs is free software; you can 
+//!    redistribute it and/or modify it
+//!    under the terms of the GNU General Public License as published by the
+//!    Free Software Foundation; either version 2 of the License, or (at your
+//!    option) any later version.
+//!
+//!    thermal_hydrualics_rs is distributed in the hope 
+//!    that it will be useful, but WITHOUT
+//!    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+//!    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+//!    for more details.
+//!
+//!    This thermal hydraulics library 
+//!    contains some code copied from GeN-Foam, and OpenFOAM derivative.
+//!    This offering is not approved or endorsed by the OpenFOAM Foundation nor
+//!    OpenCFD Limited, producer and distributor of the OpenFOAM(R)software via
+//!    www.openfoam.com, and owner of the OPENFOAM(R) and OpenCFD(R) trademarks.
+//!    Nor is it endorsed by the authors and owners of GeN-Foam.
+//!
+//!    You should have received a copy of the GNU General Public License
+//!    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//!
+//! © All rights reserved. Theodore Kay Chen Ong,
+//! Singapore Nuclear Research and Safety Initiative,
+//! Per F. Peterson,
+//! University of California, Berkeley Thermal Hydraulics Laboratory
+//!
+//! Main author of the code: Theodore Kay Chen Ong, supervised by
+//! Professor Per F. Peterson
+//!
+//! Btw, I no affiliation with the Rust Foundation. 
+//!
+extern crate uom;
+use thermal_hydraulics_rs::fluid_mechanics_lib;
+mod manual_tests;
+
+use crate::manual_tests::*;
+
+
+fn main() {
+    println!("Hello, world!");
+    test_friction_factor();
+    test_dimensionless_number();
+    test_standard_pipe_calc();
+    test_custom_fldk_component();
+    test_temperature_conversion();
+    test_therminol_pipe();
+    test_therminol_fldk_custom_component();
+    factory_test();
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+fn test_friction_factor(){
+    let darcy_friction_factor = 
+        fluid_mechanics_lib::darcy(1800.0,0.0015);
+
+    println!("{}", darcy_friction_factor);
+    
+    let fldk = 
+        fluid_mechanics_lib::fldk(
+            15000.0,0.00014,10.0,5.0);
+
+    println!("{}", fldk);
+
 }
+
