16698cdb56178cc568c01d81e83ca9904712ace0
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2023-07-03 10:34:09 -0700

    added single cv node

--- src/lib/heat_transfer_lib/control_volume_calculations/heat_transfer_entities/mod.rs
+++ src/lib/heat_transfer_lib/control_volume_calculations/heat_transfer_entities/mod.rs
@@ -1,4 +1,6 @@
-use uom::si::f64::*;
+use uom::si::{f64::*, pressure::atmosphere};
+
+use crate::heat_transfer_lib::thermophysical_properties::{Material, specific_enthalpy::specific_enthalpy};
 
 /// Contains entities which transfer heat and interact with each 
 /// other
@@ -65,13 +67,59 @@ pub enum BCTypes {
 ///
 /// 
 ///
-#[derive(Debug,Clone,Copy,PartialEq)]
+#[derive(Debug,Clone,PartialEq)]
 pub struct SingleCVNode {
+
+    /// specific enthalpy at present timestep, set using 
+    /// the temperature and material type
     current_timestep_control_volume_specific_enthalpy: AvailableEnergy,
+    /// specific enthalpy at next timestep, used to calculate 
+    /// temperature
     next_timestep_specific_enthalpy: AvailableEnergy,
 
     /// contains changes to the specific enthalpy due to changes
-    enthalpy_change_vector: Energy,
+    enthalpy_change_vector: Vec<Energy>,
 
-    
+    /// control volume mass 
+    mass_control_volume: Mass,
 }
+
+impl SingleCVNode {
+    /// to initiate the control volume, use this constructor,
+    /// which means we supply the temperature, material type 
+    /// and mass of the CV
+    ///
+    /// assumes proeprties are at atmospheric pressure
+    pub fn new(cv_temperature: ThermodynamicTemperature,
+        cv_material: Material,
+        cv_mass: Mass) -> SingleCVNode {
+
+        let atmospheric_pressure = Pressure::new::<atmosphere>(1.0);
+
+        let cv_enthalpy: AvailableEnergy = 
+        match specific_enthalpy(
+            cv_material, 
+            cv_temperature, 
+            atmospheric_pressure) {
+                Ok(specific_enthalpy) => specific_enthalpy,
+                Err(error_msg) => panic!("{}", error_msg),
+
+        };
+
+        let cv_enthalpy_change_vec: Vec<Energy> = vec![];
+
+        return Self{
+            current_timestep_control_volume_specific_enthalpy : 
+            cv_enthalpy,
+            next_timestep_specific_enthalpy : 
+            cv_enthalpy,
+            enthalpy_change_vector : 
+            cv_enthalpy_change_vec,
+            mass_control_volume : 
+            cv_mass,
+        }
+
+    }
+}
+
+
