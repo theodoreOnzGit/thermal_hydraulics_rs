2b770ccfaa759e78c8f7f14e221129753243a3f4
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2023-08-01 13:58:11 -0700

    added a method in singlecv for timestep based on max temperature change

--- src/lib/heat_transfer_lib/control_volume_calculations/heat_transfer_entities/mod.rs
+++ src/lib/heat_transfer_lib/control_volume_calculations/heat_transfer_entities/mod.rs
@@ -4,7 +4,7 @@ use uom::si::{f64::*, pressure::atmosphere, power::watt, time::second, length::m
 
 use crate::heat_transfer_lib::
 thermophysical_properties::{Material, 
-    specific_enthalpy::{specific_enthalpy, temperature_from_specific_enthalpy}, density::density, thermal_diffusivity::thermal_diffusivity};
+    specific_enthalpy::{specific_enthalpy, temperature_from_specific_enthalpy}, density::density, thermal_diffusivity::thermal_diffusivity, specific_heat_capacity::specific_heat_capacity};
 
 /// Contains entities which transfer heat and interact with each 
 /// other
@@ -470,7 +470,12 @@ impl SingleCVNode {
     }
 
     /// this is a function to determine the relevant time scales 
-    /// dynamically 
+    /// this one is based on conduction, which calculates timescales 
+    /// based on mesh fourier number
+    /// for stable conduction
+    ///
+    /// for an uneven volume, it will just take the shortest of 
+    /// these lengthscales to determine a proper time scale
     #[inline]
     fn calculate_conduction_timestep(&self) -> Result<Time,String>{
 
@@ -520,6 +525,105 @@ impl SingleCVNode {
 
 
         return Ok(min_conduction_timescale);
+    }
+
+    /// calculates a time scale based on maximum temperature change 
+    /// within one time step, 
+    #[inline]
+    fn get_max_timestep_based_on_max_temperature_change(
+        &self,
+        max_temperature_change: TemperatureInterval) 
+    -> Result<Time,String> {
+
+        // let's get the net power change first 
+
+        // first thing is to sum up all enthalpy rate of change 
+        let mut total_enthalpy_rate_change = 
+        Power::new::<watt>(0.0);
+
+        for enthalpy_chg_rate in 
+            self.rate_enthalpy_change_vector.clone().iter() {
+
+                total_enthalpy_rate_change += *enthalpy_chg_rate;
+            }
+
+        // we have Q = m c_p Delta T
+        // Q = power * time 
+        //
+        // power * time = m c_p  Delta T
+        //
+        // time = m c_p Delta T / power 
+        // for small temperature changes, assume cp constant
+
+        let cv_mass_clone = self.mass_control_volume.clone();
+        let cv_material = self.material_control_volume.clone();
+        let cv_temperature = self.get_temperature()?;
+        let cv_pressure = self.pressure_control_volume.clone();
+
+        let cv_heat_capacity = specific_heat_capacity(
+            cv_material,
+            cv_temperature,
+            cv_pressure,
+        )?;
+
+        // now we can calculate time step
+
+        let time_step_based_on_max_temperature_change = 
+        cv_mass_clone * cv_heat_capacity * max_temperature_change / 
+        total_enthalpy_rate_change;
+
+        // now return it to the environment 
+
+        return Ok(time_step_based_on_max_temperature_change);
+
+    }
+
+    /// compiles a list of time steps based on various criteria, 
+    ///
+    ///
+    #[inline]
+    pub fn get_max_timestep(&mut self, 
+        max_temperature_change: TemperatureInterval) -> Result<Time, String> {
+
+        // let's calculate conduction time step first, based on 
+        // thermal diffusivity and fourier number
+        let conduction_timestep = self.calculate_conduction_timestep()?;
+
+        // next I want to push it to the vector 
+
+        self.max_timestep_vector.push(conduction_timestep);
+
+        // let's match the material to solid or liquid 
+
+        let cv_material = self.material_control_volume.clone();
+
+        match cv_material {
+            Material::Solid(_) => (),
+            Material::Liquid(_) => todo!("need to calculate liquid timestep"),
+        }
+
+        // last but not least, check if temperature changes exceed a 
+        // certain mount, recommend 0.5 C or 1 C for max temperature 
+        // change in one time step
+        
+        let max_temperature_change_timestep: Time = 
+        self.get_max_timestep_based_on_max_temperature_change(
+            max_temperature_change)?;
+
+        self.max_timestep_vector.push(max_temperature_change_timestep);
+
+
+        // initiate a simple loop to find the shortest time scale
+        // that is "safe" out of all time steps
+        let mut min_timescale = Time::new::<second>(100_f64);
+
+        for time in self.max_timestep_vector.iter() {
+            if *time < min_timescale {
+                min_timescale = *time;
+            }
+        }
+        return Ok(min_timescale);
+
     }
 
 }
