28375010befc2b0082c454a5a6d2a14d6bebf38e
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2023-09-12 09:25:40 -0700

    preprocessing methods added template

--- src/examples/ciet/heater/heater_version_2_bare/preprocessing.rs
+++ src/examples/ciet/heater/heater_version_2_bare/preprocessing.rs
@@ -1,7 +1,17 @@
 use super::HeaterVersion2Bare;
+use thermal_hydraulics_rs::prelude::alpha_nightly::*;
 
 impl HeaterVersion2Bare {
-    
+
+    pub fn get_air_steel_shell_conductance(&mut self) 
+        -> ThermalConductance {
+        todo!()
+    }
+
+    pub fn get_therminol_node_steel_shell_conductance(&mut self) 
+        -> ThermalConductance {
+        todo!()
+    }
 }
 
 
