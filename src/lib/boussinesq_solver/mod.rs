dc7ee3aa3024f90b12c51c93e145492424b7fd33
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-03-07 21:39:00 +0800

    added some todos for the thermophysical properties consolidation

--- src/lib/boussinesq_solver/mod.rs
+++ src/lib/boussinesq_solver/mod.rs
@@ -1,3 +1,7 @@
 /// Module specifically for thermophysical properties
 /// For liquids and solids with almost invariable density
+///
+/// TODO: 
+/// 1) need to implement range checker for all properties
+/// 2) also, consolidate dowtherm A / Therminol VP1 properties
 pub mod boussinesq_thermophysical_properties;
