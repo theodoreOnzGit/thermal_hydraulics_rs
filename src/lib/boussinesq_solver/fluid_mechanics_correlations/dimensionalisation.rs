3f4200022f691b079bbda672fe4ec5c547c03ce8
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-03-12 10:08:41 +0800

    cleaned up code for dimensionalisation

--- src/lib/boussinesq_solver/fluid_mechanics_correlations/dimensionalisation.rs
+++ src/lib/boussinesq_solver/fluid_mechanics_correlations/dimensionalisation.rs
@@ -62,178 +62,148 @@ use crate::thermal_hydraulics_error::ThermalHydraulicsLibError;
 // Btw, I have no affiliation with the Rust Foundation.
 
 
-/// first and most important function here,
-/// this function converts a quantity into float given the correct dimensions
-/// if it's not dimensionless, it will throw a compile time error
-pub fn convert_dimensionless_number_to_float(dimensionless_number: Ratio) -> f64 {
-    return dimensionless_number.value.into();
+
+
+pub fn calc_reynolds_from_velocity(fluid_density: MassDensity,
+    velocity: Velocity, 
+    hydraulic_diameter: Length,
+    fluid_viscosity: DynamicViscosity) -> Ratio {
+
+    if fluid_viscosity.value <= 0.0 {
+        panic!("fluid Viscosity <= 0.0, nonphysical");
+    }
+
+    if hydraulic_diameter.value <= 0.0 {
+        panic!("hydraulic Diameter <= 0.0, nonphysical");
+    }
+    if fluid_density.value <= 0.0 {
+        panic!("fluidDensity <= 0.0, nonphysical");
+    }
+
+    let reynolds_number = 
+        fluid_density * 
+        velocity * 
+        hydraulic_diameter / 
+        fluid_viscosity;
+
+    reynolds_number
+
 }
 
 
-/// struct which contains associated functions to calculate Re
-///
-/// I might want to turn this into a trait or make a trait for this 
-/// in future
-pub struct CalcReynolds {}
+/// calculates Re = mass_flow/area * D_H/mu
+pub fn calc_reynolds_from_mass_rate(fluid_mass_flowrate: MassRate,
+    cross_sectional_area: Area,
+    hydraulic_diameter: Length,
+    fluid_viscosity: DynamicViscosity) -> Ratio {
 
-impl CalcReynolds {
-    /// calculates Re = rho * U * D /mu
-    #[allow(non_snake_case)]
-    pub fn from_velocity(fluidDensity: MassDensity,
-                     velocity: Velocity, 
-                     hydraulic_diameter: Length,
-                     fluidViscosity: DynamicViscosity) -> f64 {
-
-        if fluidViscosity.value <= 0.0 {
-            panic!("fluid Viscosity <= 0.0, nonphysical");
-        }
-
-        if hydraulic_diameter.value <= 0.0 {
-            panic!("hydraulic Diameter <= 0.0, nonphysical");
-        }
-        if fluidDensity.value <= 0.0 {
-            panic!("fluidDensity <= 0.0, nonphysical");
-        }
-
-        let reynolds_number = 
-            fluidDensity * 
-            velocity * 
-            hydraulic_diameter / 
-            fluidViscosity;
-
-
-
-        return convert_dimensionless_number_to_float(reynolds_number);
-
+    if fluid_viscosity.value <= 0.0 {
+        panic!("fluid Viscosity <= 0.0, nonphysical");
     }
 
-
-    #[allow(non_snake_case)]
-    /// calculates Re = mass_flow/area * D_H/mu
-    pub fn from_mass_rate(fluidMassFlowrate: MassRate,
-                        crossSectionalArea: Area,
-                        hydraulic_diameter: Length,
-                        fluidViscosity: DynamicViscosity) -> f64 {
-
-        if fluidViscosity.value <= 0.0 {
-            panic!("fluid Viscosity <= 0.0, nonphysical");
-        }
-
-        if hydraulic_diameter.value <= 0.0 {
-            panic!("hydraulic Diameter <= 0.0, nonphysical");
-        }
-        if crossSectionalArea.value <= 0.0 {
-            panic!("pipe Area <= 0.0, nonphysical");
-        }
-
-        let reynolds_number = fluidMassFlowrate/
-            crossSectionalArea*
-            hydraulic_diameter/
-            fluidViscosity;
-
-
-        return convert_dimensionless_number_to_float(reynolds_number);
+    if hydraulic_diameter.value <= 0.0 {
+        panic!("hydraulic Diameter <= 0.0, nonphysical");
+    }
+    if cross_sectional_area.value <= 0.0 {
+        panic!("pipe Area <= 0.0, nonphysical");
     }
 
-    
-    #[allow(non_snake_case)]
-    /// converts Re to mass flowrate using
-    /// Re = mass_flow/area * D_H/mu
-    pub fn to_mass_rate(crossSectionalArea: Area,
-                        Re: f64,
-                        hydraulicDiameter: Length,
-                        fluidViscosity: DynamicViscosity) -> MassRate {
+    let reynolds_number = fluid_mass_flowrate/
+        cross_sectional_area*
+        hydraulic_diameter/
+        fluid_viscosity;
 
-        if fluidViscosity.value <= 0.0 {
-            panic!("fluid Viscosity <= 0.0, nonphysical");
-        }
 
-        if hydraulicDiameter.value <= 0.0 {
-            panic!("hydraulic Diameter <= 0.0, nonphysical");
-        }
-
-        if crossSectionalArea.value <= 0.0 {
-            panic!("pipe Area <= 0.0, nonphysical");
-        }
-
-        let fluidMassFlowrate = fluidViscosity*
-            crossSectionalArea/
-            hydraulicDiameter*
-            Re;
-
-        return fluidMassFlowrate;
-    }
+    reynolds_number
 }
 
-/// contains assoc functions which help calculate bejan
-/// number,
-///
-/// in our context, it is a form of dimensionless pressure
-///
-/// i might want to make traits out of this in future
-pub struct CalcBejan {}
-impl CalcBejan {
 
-    #[allow(non_snake_case)]
-    /// calculates Bejan number from pressure
-    ///
-    /// Be_D = Delta P * rho * D_H^2 / mu^2
-    pub fn from_pressure(fluidPressure: Pressure,
-              hydraulicDiameter: Length,
-              fluidDensity: MassDensity,
-              fluidViscosity: DynamicViscosity) -> f64 {
+/// converts Re to mass flowrate using
+/// Re = mass_flow/area * D_H/mu
+pub fn calc_reynolds_to_mass_rate(cross_sectional_area: Area,
+    reynolds_number: Ratio,
+    hydraulic_diameter: Length,
+    fluid_viscosity: DynamicViscosity) -> MassRate {
 
-
-        if fluidViscosity.value <= 0.0 {
-            panic!("fluid Viscosity <= 0.0, nonphysical");
-        }
-
-        if hydraulicDiameter.value <= 0.0 {
-            panic!("hydraulic Diameter <= 0.0, nonphysical");
-        }
-
-        if fluidDensity.value <= 0.0 {
-            panic!("fluidDensity <= 0.0, nonphysical");
-        }
-
-        let Be = fluidPressure*
-            fluidDensity *
-            hydraulicDiameter.powi(P2::new())/
-            fluidViscosity.powi(P2::new());
-
-        return convert_dimensionless_number_to_float(Be);
+    if fluid_viscosity.value <= 0.0 {
+        panic!("fluid Viscosity <= 0.0, nonphysical");
     }
 
-    #[allow(non_snake_case)]
-    /// converts Bejan number to pressure
-    /// using:
-    ///
-    /// Be_D = Delta P * rho * D_H^2 / mu^2
-    pub fn to_pressure(Be_D: f64,
-                       hydraulicDiameter: Length,
-                       fluidDensity: MassDensity,
-                       fluidViscosity: DynamicViscosity) -> Pressure {
-
-
-        if fluidViscosity.value <= 0.0 {
-            panic!("fluid Viscosity <= 0.0, nonphysical");
-        }
-
-        if hydraulicDiameter.value <= 0.0 {
-            panic!("hydraulic Diameter <= 0.0, nonphysical");
-        }
-
-        if fluidDensity.value <= 0.0 {
-            panic!("fluidDensity <= 0.0, nonphysical");
-        }
-
-        let fluidPressure = fluidViscosity.powi(P2::new())*
-                        Be_D/
-                        hydraulicDiameter.powi(P2::new())/
-                        fluidDensity;
-
-        return fluidPressure;
+    if hydraulic_diameter.value <= 0.0 {
+        panic!("hydraulic Diameter <= 0.0, nonphysical");
     }
 
+    if cross_sectional_area.value <= 0.0 {
+        panic!("pipe Area <= 0.0, nonphysical");
+    }
 
+    let fluid_mass_flowrate = fluid_viscosity*
+        cross_sectional_area/
+        hydraulic_diameter*
+        reynolds_number;
+
+    return fluid_mass_flowrate;
 }
+
+
+/// calculates Bejan number from pressure
+///
+/// Be_D = Delta P * rho * D_H^2 / mu^2
+pub fn calc_bejan_from_pressure(fluid_pressure: Pressure,
+    hydraulic_diameter: Length,
+    fluid_density: MassDensity,
+    fluid_viscosity: DynamicViscosity) -> Ratio {
+
+
+    if fluid_viscosity.value <= 0.0 {
+        panic!("fluid Viscosity <= 0.0, nonphysical");
+    }
+
+    if hydraulic_diameter.value <= 0.0 {
+        panic!("hydraulic Diameter <= 0.0, nonphysical");
+    }
+
+    if fluid_density.value <= 0.0 {
+        panic!("fluidDensity <= 0.0, nonphysical");
+    }
+
+    let bejan_number_d = fluid_pressure*
+        fluid_density *
+        hydraulic_diameter.powi(P2::new())/
+        fluid_viscosity.powi(P2::new());
+
+    bejan_number_d
+}
+
+/// converts Bejan number to pressure
+/// using:
+///
+/// Be_D = Delta P * rho * D_H^2 / mu^2
+pub fn calc_bejan_to_pressure(bejan_d: f64,
+    hydraulic_diameter: Length,
+    fluid_density: MassDensity,
+    fluid_viscosity: DynamicViscosity) -> Pressure {
+
+
+    if fluid_viscosity.value <= 0.0 {
+        panic!("fluid Viscosity <= 0.0, nonphysical");
+    }
+
+    if hydraulic_diameter.value <= 0.0 {
+        panic!("hydraulic Diameter <= 0.0, nonphysical");
+    }
+
+    if fluid_density.value <= 0.0 {
+        panic!("fluidDensity <= 0.0, nonphysical");
+    }
+
+    let fluid_pressure = fluid_viscosity.powi(P2::new())*
+        bejan_d/
+        hydraulic_diameter.powi(P2::new())/
+        fluid_density;
+
+    return fluid_pressure;
+}
+
+
+
