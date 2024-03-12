2a64136df37afc9eb6d02a782f740268d3067ae9
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-03-12 13:16:20 +0800

    added thermal resistance calculations

--- src/lib/boussinesq_solver/heat_transfer_correlations/mod.rs
+++ src/lib/boussinesq_solver/heat_transfer_correlations/mod.rs
@@ -1,2 +1,5 @@
 /// correlations for convective heat transfer
 pub mod nusselt_number_correlations;
+
+/// calculation functions for thermal resistance
+pub mod thermal_resistance;
--- src/lib/boussinesq_solver/mod.rs
+++ src/lib/boussinesq_solver/mod.rs
@@ -1,3 +1,4 @@
+#![warn(missing_docs)]
 /// Module specifically for thermophysical properties
 /// For liquids and solids with almost invariable density
 ///
@@ -13,3 +14,18 @@ pub mod fluid_mechanics_correlations;
 /// suitable for boussinesq_solver (single phase, negligble density changes
 /// except for buoyancy)
 pub mod heat_transfer_correlations;
+
+/// Module for solid control volumes (both singular control volumes 
+/// and arrays of control volumes)
+pub mod solid_control_vol;
+
+/// Module for fluid control volumes (both singular control volumes 
+/// and arrays of control volumes)
+/// suitable for boussinesq_solver (single phase, negligble density changes
+/// except for buoyancy)
+pub mod fluid_control_vol;
+
+/// Module for pre-built-components 
+/// suitable for boussinesq_solver (single phase, negligble density changes
+/// except for buoyancy)
+pub mod pre_built_components;
