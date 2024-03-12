6cdabed5811719e6f489a08dcc6de0b1ff9d8c37
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-03-12 09:16:59 +0800

    first import of fluid mechanics correlations, pending test overwriting

--- src/lib/boussinesq_solver/mod.rs
+++ src/lib/boussinesq_solver/mod.rs
@@ -1,6 +1,8 @@
 /// Module specifically for thermophysical properties
 /// For liquids and solids with almost invariable density
 ///
-/// TODO: 
-/// 1) need to implement range checker for density? Ranges not given
 pub mod boussinesq_thermophysical_properties;
+
+/// Module for correlations of fluid mechanics 
+pub mod fluid_mechanics_correlations;
+
