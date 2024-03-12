ccc7a625082d732d2387646709188e04ae68e02c
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-03-12 10:10:09 +0800

    added comments for boussinesq solver

--- src/lib/boussinesq_solver/mod.rs
+++ src/lib/boussinesq_solver/mod.rs
@@ -4,5 +4,7 @@
 pub mod boussinesq_thermophysical_properties;
 
 /// Module for correlations of fluid mechanics 
+/// suitable for boussinesq_solver (single phase, negligble density changes
+/// except for buoyancy)
 pub mod fluid_mechanics_correlations;
 
