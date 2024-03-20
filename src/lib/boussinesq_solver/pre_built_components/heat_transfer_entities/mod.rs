bc6adc79b9369235924bf5dad900dd5badc3c5d2
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-03-20 10:48:57 +0800

    implemented some array cv methods

--- src/lib/boussinesq_solver/pre_built_components/heat_transfer_entities/mod.rs
+++ src/lib/boussinesq_solver/pre_built_components/heat_transfer_entities/mod.rs
@@ -1,6 +1,9 @@
 use crate::boussinesq_solver::array_control_vol_and_fluid_component_collections::one_d_solid_array_with_lateral_coupling::SolidColumn;
+use crate::boussinesq_solver::boussinesq_thermophysical_properties::Material;
 use crate::boussinesq_solver::single_control_vol::SingleCVNode;
 use crate::boussinesq_solver::array_control_vol_and_fluid_component_collections::one_d_fluid_array_with_lateral_coupling::FluidArray;
+use crate::thermal_hydraulics_error::ThermalHydraulicsLibError;
+use uom::si::f64::*;
 
 
 /// Contains Types of Control Volumes (CVs)
@@ -42,5 +45,49 @@ impl From<FluidArray> for CVType {
 impl From<SolidColumn> for CVType {
     fn from(solid_array: SolidColumn) -> Self {
         Self::SolidArrayCV(solid_array)
+    }
+}
+
+impl CVType {
+    #[inline]
+    /// gets the material 
+    pub fn get_material(&mut self) -> Result<Material,ThermalHydraulicsLibError>{
+
+
+        match self {
+            CVType::SingleCV(single_cv_node) => {
+                return Ok(single_cv_node.material_control_volume);
+            },
+            CVType::FluidArrayCV(fluid_array_cv) => {
+                return Ok(fluid_array_cv.front_single_cv.material_control_volume);
+            },
+            CVType::SolidArrayCV(solid_array_cv) => {
+                return Ok(solid_array_cv.front_single_cv.material_control_volume);
+            },
+        }
+    }
+
+    #[inline]
+    fn get_temperature_vector(&mut self) -> 
+    Result<Vec<ThermodynamicTemperature>,ThermalHydraulicsLibError>{
+        match self {
+            CVType::SingleCV(single_cv) => {
+                let temperature = single_cv.get_temperature_from_enthalpy_and_set()?;
+
+                let mut temp_vec: Vec<ThermodynamicTemperature> = vec![];
+
+                temp_vec.push(temperature);
+
+                return Ok(temp_vec);
+
+            },
+            CVType::FluidArrayCV(fluid_array_cv) => {
+                return fluid_array_cv.get_temperature_vector();
+            },
+            CVType::SolidArrayCV(solid_array_cv) => {
+                return solid_array_cv.get_temperature_vector();
+            },
+
+        }
     }
 }
