39b3e7dfd9821f3a14bac0389d938ed279eb7776
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-03-20 15:05:10 +0800

    added bc types

--- src/lib/boussinesq_solver/pre_built_components/heat_transfer_entities/mod.rs
+++ src/lib/boussinesq_solver/pre_built_components/heat_transfer_entities/mod.rs
@@ -1,6 +1,10 @@
-
 /// all the types of Control volumes are represented in an enum 
 /// to abstract away the complications of connecting different types 
 /// of control volumes. 
 pub mod cv_types;
+
+/// all the types of boundary conditions are represented in an enum 
+/// to abstract away the complications of connecting different types 
+/// of boundary conditions
+pub mod bc_types;
 
