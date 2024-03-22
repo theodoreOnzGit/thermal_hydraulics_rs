a8fbc918230e64852c804280b33bf1b61729239f
teddy0-mint-box <theodore_ong@berkeley.edu>
2024-03-22 10:25:05 +0800

    added constructor for new pipe

--- src/lib/boussinesq_solver/pre_built_components/pipes/mod.rs
+++ src/lib/boussinesq_solver/pre_built_components/pipes/mod.rs
@@ -1,9 +1,18 @@
+use crate::boussinesq_solver::array_control_vol_and_fluid_component_collections::one_d_fluid_array_with_lateral_coupling::FluidArray;
+use crate::boussinesq_solver::array_control_vol_and_fluid_component_collections::one_d_solid_array_with_lateral_coupling::SolidColumn;
+use crate::boussinesq_solver::boussinesq_thermophysical_properties::SolidMaterial;
+use crate::boussinesq_solver::boussinesq_thermophysical_properties::LiquidMaterial;
+
+use super::heat_transfer_entities::cv_types::CVType;
 use super::heat_transfer_entities::HeatTransferEntity;
 use uom::si::f64::*;
 
 /// The simplest component is a non insulated pipe
 ///
 /// This is a simple pipe with a set hydraulic diameter and length
+///
+/// the standard assumption is that at each boundary of this pipe,
+/// there is no conduction heat transfer in the axial direction
 pub struct NonInsulatedPipe {
 
     inner_nodes: usize,
@@ -28,4 +37,82 @@ pub struct NonInsulatedPipe {
     /// pipe heat transfer coefficient to ambient
     pub heat_transfer_to_ambient: HeatTransfer,
 
+}
+
+impl NonInsulatedPipe {
+
+    /// constructs a new pipe
+    ///
+    /// you need to supply the initial temperature, ambient temperature
+    /// as well as all the pipe parameters 
+    ///
+    /// such as:
+    ///
+    /// 1. flow area 
+    /// 2. hydraulic diameter 
+    /// 3. incline angle
+    /// 4. any form losses beyond the Gnielinski correlation
+    /// 5. inner diameter (id)
+    /// 6. outer diameter (od)
+    /// 7. pipe shell material 
+    /// 8. pipe fluid 
+    /// 9. fluid pressure (if in doubt, 1 atmosphere will do)
+    /// 10. solid pressure (if in doubt, 1 atmosphere will do)
+    /// 11. heat transfer coeffficient to ambient
+    /// 12. how many inner axial nodes for both solid and fluid arrays
+    ///
+    /// The number of total axial nodes is the number of inner nodes plus 2
+    ///
+    /// this is because there are two nodes at the periphery of the pipe 
+    /// and there
+    pub fn new_pipe(initial_temperature: ThermodynamicTemperature,
+        ambient_temperature: ThermodynamicTemperature,
+        fluid_pressure: Pressure,
+        solid_pressure: Pressure,
+        flow_area: Area,
+        incline_angle: Angle,
+        form_loss: Ratio,
+        id: Length,
+        od: Length,
+        pipe_length: Length,
+        hydraulic_diameter: Length,
+        pipe_shell_material: SolidMaterial,
+        pipe_fluid: LiquidMaterial,
+        htc_to_ambient: HeatTransfer,
+        user_specified_inner_nodes: usize) -> NonInsulatedPipe {
+
+        // inner fluid_array
+        let fluid_array: FluidArray = 
+        FluidArray::new_odd_shaped_pipe(
+            pipe_length,
+            hydraulic_diameter,
+            flow_area,
+            initial_temperature,
+            fluid_pressure,
+            pipe_shell_material,
+            pipe_fluid,
+            form_loss,
+            user_specified_inner_nodes,
+            incline_angle
+        );
+
+        // now the outer steel array
+        let pipe_shell = 
+        SolidColumn::new_cylindrical_shell(
+            pipe_length,
+            id,
+            od,
+            initial_temperature,
+            solid_pressure,
+            pipe_shell_material,
+            user_specified_inner_nodes 
+        );
+
+        return Self { inner_nodes: user_specified_inner_nodes,
+            pipe_shell: CVType::SolidArrayCV(pipe_shell).into(),
+            pipe_fluid_array: CVType::FluidArrayCV(fluid_array).into(),
+            ambient_temperature,
+            heat_transfer_to_ambient: htc_to_ambient,
+        };
+    }
 }
