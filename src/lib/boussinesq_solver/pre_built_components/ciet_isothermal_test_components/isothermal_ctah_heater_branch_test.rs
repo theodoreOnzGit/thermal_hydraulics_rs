fa154aac105fc835a41b1a0e69d63685e45fd46b
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2024-04-22 14:01:49 +0800

    ascertained that a significant problem lies within the ctah branch

--- src/lib/boussinesq_solver/pre_built_components/ciet_isothermal_test_components/isothermal_ctah_heater_branch_test.rs
+++ src/lib/boussinesq_solver/pre_built_components/ciet_isothermal_test_components/isothermal_ctah_heater_branch_test.rs
@@ -244,6 +244,44 @@ pub fn heater_branch_pressure_change_test(){
 
 }
 
+#[test] 
+pub fn ctah_branch_pressure_change_test(){
+
+    // let's construct the branches with test pressures and obtain 
+    // mass flowrates
+    use uom::si::f64::*;
+    use super::ciet_branch_builders_isothermal::*;
+    use uom::ConstZero;
+    use uom::si::mass_rate::kilogram_per_second;
+    use approx::assert_abs_diff_eq;
+    use uom::si::pressure::pascal;
+
+    let pump_pressure = Pressure::ZERO;
+    let heater_branch = ctah_branch_builder_isothermal_test(
+        pump_pressure);
+
+    // pressure change at 0 kg/s 
+    let pressure_change_at_zero_kg_per_s = 
+        heater_branch.get_pressure_change(MassRate::ZERO);
+
+    // pressure change should be 39041 +/- 1 Pa
+    assert_abs_diff_eq!(pressure_change_at_zero_kg_per_s.get::<pascal>(), 
+        39041.0, epsilon=1.0,);
+
+    // now at 0.18 kg/s
+    // pressure change at 0 kg/s 
+    let pressure_change_at_0_18_kg_per_s = 
+        heater_branch.get_pressure_change(
+            MassRate::new::<kilogram_per_second>(0.18));
+
+    // pressure change should be 28751.0 +/- 100 Pa
+    // this is based on the old heater branch values 
+    // form the old ciet isothermal server
+    assert_abs_diff_eq!(pressure_change_at_0_18_kg_per_s.get::<pascal>(), 
+        28751.0, epsilon=100.0,);
+
+
+}
 
 #[test]
 pub fn isothermal_ctah_and_heater_branch_validation_test(){
