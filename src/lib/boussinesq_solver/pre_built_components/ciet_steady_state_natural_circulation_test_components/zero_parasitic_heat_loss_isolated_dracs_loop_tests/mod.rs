3b05531fcdeae7adc11d64b953f694d6ee6ff91a
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2024-04-23 16:35:48 +0800

    added which test dataset corresponds to which solution

--- src/lib/boussinesq_solver/pre_built_components/ciet_steady_state_natural_circulation_test_components/zero_parasitic_heat_loss_isolated_dracs_loop_tests/mod.rs
+++ src/lib/boussinesq_solver/pre_built_components/ciet_steady_state_natural_circulation_test_components/zero_parasitic_heat_loss_isolated_dracs_loop_tests/mod.rs
@@ -2,7 +2,35 @@ use uom::si::{f64::*, mass_rate::kilogram_per_second, power::watt};
 
 use crate::prelude::beta_testing::ThermalHydraulicsLibError;
 #[test]
-pub fn case_a(){
+
+/// In the original SAM publication
+///
+/// Zou, L., Hu, R., & Charpentier, A. (2019). SAM code 
+/// validation using the compact integral effects test (CIET) experimental 
+/// data (No. ANL/NSE-19/11). Argonne National 
+/// Lab.(ANL), Argonne, IL (United States).
+///
+/// I found it hard to distinguish what TCHX temperatures case A,
+/// B and C were.
+///
+/// But there was another publication which shows which is test group 
+/// corresponds to which temperature:
+///
+/// Zou, L., Hu, G., O'Grady, D., & Hu, R. (2021). Code validation of 
+/// SAM using natural-circulation experimental data from the compact 
+/// integral effects test (CIET) facility. 
+/// Nuclear Engineering and Design, 377, 111144.
+///
+/// According to table 2,
+///
+/// Case A has 7 tests and TCHX out temperature of 46 C
+/// Case B has 9 tests and TCHX out temperature of 35 C
+/// Case C has 9 tests and TCHX out temperature of 40 C
+///
+/// Table 3 also provides the data 
+/// 
+///
+pub fn case_a_tchx_out_319_kelvin_46_celsius(){
 
 
     verify_isolated_dhx_analytical_solution(
@@ -36,8 +64,35 @@ pub fn case_a(){
 }
 
 
+/// In the original SAM publication
+///
+/// Zou, L., Hu, R., & Charpentier, A. (2019). SAM code 
+/// validation using the compact integral effects test (CIET) experimental 
+/// data (No. ANL/NSE-19/11). Argonne National 
+/// Lab.(ANL), Argonne, IL (United States).
+///
+/// I found it hard to distinguish what TCHX temperatures case A,
+/// B and C were.
+///
+/// But there was another publication which shows which is test group 
+/// corresponds to which temperature:
+///
+/// Zou, L., Hu, G., O'Grady, D., & Hu, R. (2021). Code validation of 
+/// SAM using natural-circulation experimental data from the compact 
+/// integral effects test (CIET) facility. 
+/// Nuclear Engineering and Design, 377, 111144.
+///
+/// According to table 2,
+///
+/// Case A has 7 tests and TCHX out temperature of 46 C
+/// Case B has 9 tests and TCHX out temperature of 35 C
+/// Case C has 9 tests and TCHX out temperature of 40 C
+///
+/// Table 3 also provides the data 
+/// 
+///
 #[test]
-pub fn case_b(){
+pub fn case_b_tchx_out_308_kelvin_35_celsius(){
 
     verify_isolated_dhx_analytical_solution(
         454.4, 
@@ -77,8 +132,35 @@ pub fn case_b(){
         ).unwrap();
 }
 
+/// In the original SAM publication
+///
+/// Zou, L., Hu, R., & Charpentier, A. (2019). SAM code 
+/// validation using the compact integral effects test (CIET) experimental 
+/// data (No. ANL/NSE-19/11). Argonne National 
+/// Lab.(ANL), Argonne, IL (United States).
+///
+/// I found it hard to distinguish what TCHX temperatures case A,
+/// B and C were.
+///
+/// But there was another publication which shows which is test group 
+/// corresponds to which temperature:
+///
+/// Zou, L., Hu, G., O'Grady, D., & Hu, R. (2021). Code validation of 
+/// SAM using natural-circulation experimental data from the compact 
+/// integral effects test (CIET) facility. 
+/// Nuclear Engineering and Design, 377, 111144.
+///
+/// According to table 2,
+///
+/// Case A has 7 tests and TCHX out temperature of 46 C
+/// Case B has 9 tests and TCHX out temperature of 35 C
+/// Case C has 9 tests and TCHX out temperature of 40 C
+///
+/// Table 3 also provides the data 
+/// 
+///
 #[test]
-pub fn case_c(){
+pub fn case_c_tchx_out_313_kelvin_40_celsius(){
 
     verify_isolated_dhx_analytical_solution(
         582.6, 
@@ -119,7 +201,7 @@ pub fn case_c(){
 }
 
 /// function to verify the dhx analytical solution
-fn verify_isolated_dhx_analytical_solution(
+pub fn verify_isolated_dhx_analytical_solution(
     input_power_watts: f64,
     analytical_solution_mass_flowrate_kg_per_s: f64) -> 
 Result<(),ThermalHydraulicsLibError>{
