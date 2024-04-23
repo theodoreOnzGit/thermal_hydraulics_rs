b4fa80ca8634beb05c1a28b0be336af79902dbc8
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2024-04-23 16:39:21 +0800

    copied documentation

--- src/lib/boussinesq_solver/pre_built_components/ciet_steady_state_natural_circulation_test_components/mod.rs
+++ src/lib/boussinesq_solver/pre_built_components/ciet_steady_state_natural_circulation_test_components/mod.rs
@@ -15,6 +15,33 @@
 ///
 /// These are enforced as part of the boundary conditions of the TCHX
 ///
+/// In the original SAM publication
+///
+/// Zou, L., Hu, R., & Charpentier, A. (2019). SAM code 
+/// validation using the compact integral effects test (CIET) experimental 
+/// data (No. ANL/NSE-19/11). Argonne National 
+/// Lab.(ANL), Argonne, IL (United States).
+///
+/// I found it hard to distinguish what TCHX temperatures case A,
+/// B and C were.
+///
+/// But there was another publication which shows which is test group 
+/// corresponds to which temperature:
+///
+/// Zou, L., Hu, G., O'Grady, D., & Hu, R. (2021). Code validation of 
+/// SAM using natural-circulation experimental data from the compact 
+/// integral effects test (CIET) facility. 
+/// Nuclear Engineering and Design, 377, 111144.
+///
+/// According to table 2,
+///
+/// Case A has 7 tests and TCHX out temperature of 46 C
+/// Case B has 9 tests and TCHX out temperature of 35 C
+/// Case C has 9 tests and TCHX out temperature of 40 C
+///
+/// Table 3 also provides the data for these tests. These are included 
+/// in the module
+///
 /// For this set of tests, we do not worry about real-time calculations 
 /// just yet
 ///
