5ff0c12a6fee351d46241b1703bf3a6f5c98811a
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2024-07-16 11:16:38 +0800

    added a template for flibe salt

--- src/lib/boussinesq_solver/boussinesq_thermophysical_properties/liquid_database/mod.rs
+++ src/lib/boussinesq_solver/boussinesq_thermophysical_properties/liquid_database/mod.rs
@@ -44,8 +44,46 @@ pub mod yd_325_heat_transfer_oil;
 /// I used the correlation from Srivastava et al. 
 pub mod sodium_potassium_equimolar_nissen_nitrate_salt;
 
+
+/// FLiBe,
+///
+/// Composition:
+/// LiF 67 mol% 
+/// BeF2 33 mol% 
+///
+/// Viscosity correlations for FLiBe may differ in composition slightly,
+/// due to different compositions of FLiBe used for different ranges 
+/// 
+/// Thermal conductivity is in the range of 1.1 W/(m K) in 873K to 1073K,
+/// and Sohal's correlation was originally for 500-650 K
+/// Which is strangely below the melting 
+/// point of flibe
+/// but based on Romatoski's data, I found that Romatoski's data 
+/// could fit Sohal's correlation to within 10% error
+/// even up to 1123 K in my PhD Thesis
+///
+/// Therefore I could use it in the whole temperature range from 
+/// 500 - 1123K .
+///
+///
+/// Ong, T. K. C. (2024). Digital Twins as Testbeds for 
+/// Iterative Simulated Neutronics Feedback Controller 
+/// Development (Doctoral dissertation, UC Berkeley).
+/// 
+///
+///
+/// Romatoski, R. R., & Hu, L. W. (2017). Fluoride salt coolant properties 
+/// for nuclear reactor applications: A review. Annals 
+/// of Nuclear Energy, 109, 635-647.
 /// properties for a custom liquid material 
 /// not covered in the database
+///
+/// Sohal, M. S., Ebner, M. A., Sabharwall, P., & Sharpe, P. (2010). 
+/// Engineering database of liquid salt thermophysical and thermochemical 
+/// properties (No. INL/EXT-10-18297). Idaho National Lab.(INL), 
+/// Idaho Falls, ID (United States).
+
+pub mod flibe;
 /// 
 /// You'll need to define your own functions for this to work
 pub mod custom_liquid_material;
