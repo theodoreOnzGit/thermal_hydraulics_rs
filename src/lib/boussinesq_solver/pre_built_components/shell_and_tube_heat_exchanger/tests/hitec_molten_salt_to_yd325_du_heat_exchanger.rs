a143daf6dfdf4e96a05a797b4a48c0ace5b2463f
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2024-08-13 13:09:08 +0800

    added test a6

--- src/lib/boussinesq_solver/pre_built_components/shell_and_tube_heat_exchanger/tests/hitec_molten_salt_to_yd325_du_heat_exchanger.rs
+++ src/lib/boussinesq_solver/pre_built_components/shell_and_tube_heat_exchanger/tests/hitec_molten_salt_to_yd325_du_heat_exchanger.rs
@@ -1118,12 +1118,42 @@ pub fn du_test_shell_and_tube_heat_exchanger_set_one(){
                 let tube_inlet_temperature = inlet_temp_oil;
                 let shell_inlet_temperature = inlet_temp_salt;
 
-                test_thread(
+                let (reynolds_num_a6,
+                    bulk_prandtl_a6,
+                    wall_prandtl_a6,
+                    nusselt_number_a6) = test_thread(
                     clone_for_test_six,
-                    tube_inlet_temperature, 
-                    shell_inlet_temperature, 
-                    m_t, 
-                    m_s,) 
+                    tube_inlet_temperature,
+                    shell_inlet_temperature,
+                    m_t,
+                    m_s);
+
+                // assert that the Reynolds, Prandtl bulk and 
+                // Prandtl wall are equal to some amount
+
+                assert_relative_eq!(
+                    reynolds_num_a6.get::<ratio>(),
+                    3792.0,
+                    max_relative = 0.01,
+                );
+
+                assert_relative_eq!(
+                    bulk_prandtl_a6.get::<ratio>(),
+                    11.9,
+                    max_relative = 0.01,
+                );
+
+                assert_relative_eq!(
+                    wall_prandtl_a6.get::<ratio>(),
+                    21.7,
+                    max_relative = 0.01,
+                );
+
+                assert_relative_eq!(
+                    nusselt_number_a6.get::<ratio>(),
+                    40.9,
+                    max_relative = 0.01,
+                );
             }
         );
 
