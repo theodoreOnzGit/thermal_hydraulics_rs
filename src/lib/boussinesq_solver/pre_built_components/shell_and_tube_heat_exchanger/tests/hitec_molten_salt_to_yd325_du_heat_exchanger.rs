f56275e42911e22c7c563a33a136d59b056e2f19
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2024-08-13 13:03:13 +0800

    hard coded test 4 asserts

--- src/lib/boussinesq_solver/pre_built_components/shell_and_tube_heat_exchanger/tests/hitec_molten_salt_to_yd325_du_heat_exchanger.rs
+++ src/lib/boussinesq_solver/pre_built_components/shell_and_tube_heat_exchanger/tests/hitec_molten_salt_to_yd325_du_heat_exchanger.rs
@@ -986,12 +986,42 @@ pub fn du_test_shell_and_tube_heat_exchanger_set_one(){
                 let tube_inlet_temperature = inlet_temp_oil;
                 let shell_inlet_temperature = inlet_temp_salt;
 
-                test_thread(
+                let (reynolds_num_a4,
+                    bulk_prandtl_a4,
+                    wall_prandtl_a4,
+                    nusselt_number_a4) = test_thread(
                     clone_for_test_four,
-                    tube_inlet_temperature, 
-                    shell_inlet_temperature, 
-                    m_t, 
-                    m_s,) 
+                    tube_inlet_temperature,
+                    shell_inlet_temperature,
+                    m_t,
+                    m_s);
+
+                // assert that the Reynolds, Prandtl bulk and 
+                // Prandtl wall are equal to some amount
+
+                assert_relative_eq!(
+                    reynolds_num_a4.get::<ratio>(),
+                    4179.0,
+                    max_relative = 0.01,
+                );
+
+                assert_relative_eq!(
+                    bulk_prandtl_a4.get::<ratio>(),
+                    11.5,
+                    max_relative = 0.01,
+                );
+
+                assert_relative_eq!(
+                    wall_prandtl_a4.get::<ratio>(),
+                    20.6,
+                    max_relative = 0.01,
+                );
+
+                assert_relative_eq!(
+                    nusselt_number_a4.get::<ratio>(),
+                    44.9,
+                    max_relative = 0.01,
+                );
             }
         );
 
