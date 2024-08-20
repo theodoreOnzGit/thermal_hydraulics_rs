cd512391ee9b66a3f25b26e2eba3da72217ba412
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-08-20 13:55:31 +0800

    added a thermophysical property test for hitec prandtl number in du's expt range

--- src/lib/boussinesq_solver/pre_built_components/shell_and_tube_heat_exchanger/tests/hitec_molten_salt_to_yd325_du_heat_exchanger/thermophsyical_property_checks.rs
+++ src/lib/boussinesq_solver/pre_built_components/shell_and_tube_heat_exchanger/tests/hitec_molten_salt_to_yd325_du_heat_exchanger/thermophsyical_property_checks.rs
@@ -37,3 +37,47 @@ pub fn prandtl_number_range_check(){
         max_relative=0.01
         );
 }
+
+
+/// "Plots" prandtl number from 200C to 240C in 5C intervals
+#[test]
+pub fn prandtl_number_range_from_200_to_240_celsius(){
+
+    use uom::si::f64::*;
+    use uom::si::thermodynamic_temperature::degree_celsius;
+    use uom::si::{pressure::atmosphere, ratio::ratio};
+
+    use crate::prelude::beta_testing::LiquidMaterial;
+
+
+    // test function
+    fn check_prandtl_at_temp(temp_celsius: f64,
+        prandtl_num: f64){
+
+        let temp: ThermodynamicTemperature = 
+            ThermodynamicTemperature::new::<degree_celsius>(temp_celsius);
+        let atmospheric_pressure = Pressure::new::<atmosphere>(1.0);
+        let hitec = LiquidMaterial::HITEC;
+        let calculated_prandtl: Ratio = 
+            hitec.try_get_prandtl_liquid(temp, atmospheric_pressure)
+            .unwrap();
+
+            approx::assert_relative_eq!(
+                calculated_prandtl.get::<ratio>(),
+                prandtl_num,
+                max_relative=0.01
+            );
+
+    }
+
+    check_prandtl_at_temp(200.0, 26.24);
+    check_prandtl_at_temp(205.0, 25.02);
+    check_prandtl_at_temp(210.0, 23.88);
+    check_prandtl_at_temp(215.0, 22.80);
+    check_prandtl_at_temp(220.0, 21.78);
+    check_prandtl_at_temp(225.0, 20.79);
+    check_prandtl_at_temp(230.0, 19.97);
+    check_prandtl_at_temp(235.0, 19.19);
+    check_prandtl_at_temp(240.0, 18.46);
+
+}
