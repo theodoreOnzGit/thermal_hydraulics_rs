cb38ed962a666c1d5fbefa66bbb9780e4429d651
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-09-09 08:36:04 +0800

    adjusted regression test time to about 3000s to speed up tests

--- src/lib/boussinesq_solver/pre_built_components/ciet_steady_state_natural_circulation_test_components/parasitic_heat_loss_regression_tests/coupled_dracs_loop_ver_1/mod.rs
+++ src/lib/boussinesq_solver/pre_built_components/ciet_steady_state_natural_circulation_test_components/parasitic_heat_loss_regression_tests/coupled_dracs_loop_ver_1/mod.rs
@@ -42,37 +42,50 @@ pub fn long_test_uncalibrated_dracs_loop(){
 #[test]
 pub fn regression_long_test_uncalibrated_dracs_loop(){
 
+    use std::thread;
+
     use regression_coupled_dracs_loop_version_1::*;
 
-    let max_simulation_time_seconds: f64 = 4000.0;
-    // expect overprediction of mass flowrates in both loops 
-    // to about 10%
-    let pri_loop_relative_tolerance = 0.1;
-    let dracs_loop_relative_tolerance = 0.1;
 
 
-    // I'm writing in this format so that the data will be easier 
-    // to copy over to csv
-    let (heater_power_watts,
-        tchx_outlet_temp_degc,
-        experimental_primary_mass_flowrate_kg_per_s,
-        experimental_dracs_mass_flowrate_kg_per_s,
-        simulated_expected_primary_mass_flowrate_kg_per_s,
-        simulated_expected_dracs_mass_flowrate_kg_per_s) 
-        = (2764.53, 40.0, 4.6990e-2, 3.5470e-2, 4.6990e-2, 3.5470e-2);
+    let thread_1 = thread::spawn(||{
+        let max_simulation_time_seconds: f64 = 3000.0;
+        // expect overprediction of mass flowrates in both loops 
+        // to about 10%
+        let pri_loop_relative_tolerance = 0.1;
+        let dracs_loop_relative_tolerance = 0.1;
+
+        // I'm writing in this format so that the data will be easier 
+        // to copy over to csv
+        let (heater_power_watts,
+            tchx_outlet_temp_degc,
+            experimental_primary_mass_flowrate_kg_per_s,
+            experimental_dracs_mass_flowrate_kg_per_s,
+            simulated_expected_primary_mass_flowrate_kg_per_s,
+            simulated_expected_dracs_mass_flowrate_kg_per_s) 
+            = (2764.53, 40.0, 4.6990e-2, 3.5470e-2, 4.9890e-2, 3.8766e-2);
+
+        dbg!(max_simulation_time_seconds,
+            pri_loop_relative_tolerance,
+            dracs_loop_relative_tolerance);
 
 
-    regression_coupled_dracs_loop_version_1(
-        heater_power_watts, 
-        max_simulation_time_seconds,
-        tchx_outlet_temp_degc,
-        experimental_primary_mass_flowrate_kg_per_s,
-        experimental_dracs_mass_flowrate_kg_per_s,
-        simulated_expected_primary_mass_flowrate_kg_per_s,
-        simulated_expected_dracs_mass_flowrate_kg_per_s,
-        pri_loop_relative_tolerance,
-        dracs_loop_relative_tolerance,
+        regression_coupled_dracs_loop_version_1(
+            heater_power_watts, 
+            max_simulation_time_seconds,
+            tchx_outlet_temp_degc,
+            experimental_primary_mass_flowrate_kg_per_s,
+            experimental_dracs_mass_flowrate_kg_per_s,
+            simulated_expected_primary_mass_flowrate_kg_per_s,
+            simulated_expected_dracs_mass_flowrate_kg_per_s,
+            pri_loop_relative_tolerance,
+            dracs_loop_relative_tolerance,
         ).unwrap();
+
+    });
+
+
+    thread_1.join().unwrap();
 }
 
 
