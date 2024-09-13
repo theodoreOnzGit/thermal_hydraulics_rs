a5bd5b716f754295ae65573546cbb8f3dee53a60
snrsi-user-linux-mint-setup-by-theo-ong <theodore_ong@berkeley.edu>
2024-09-13 15:55:33 +0800

    added outer shell cylindrical thermal resistance

--- src/lib/boussinesq_solver/pre_built_components/shell_and_tube_heat_exchanger/calibration.rs
+++ src/lib/boussinesq_solver/pre_built_components/shell_and_tube_heat_exchanger/calibration.rs
@@ -2,12 +2,12 @@ use std::f64::consts::PI;
 
 use uom::si::{f64::*, ratio::ratio};
 
-use crate::prelude::beta_testing::{SolidColumn, SolidMaterial};
+use crate::{prelude::beta_testing::{SolidColumn, SolidMaterial}, thermal_hydraulics_error::ThermalHydraulicsLibError};
 
 use super::SimpleShellAndTubeHeatExchanger;
 
 impl SimpleShellAndTubeHeatExchanger {
-    
+
     /// assuming sthe insulation is cylindrical,
     /// get the thermal resistance
     ///
@@ -54,6 +54,66 @@ impl SimpleShellAndTubeHeatExchanger {
 
 
         return log_numerator_term/denominator_term;
-        
+
+    }
+
+
+    /// assuming sthe insulation is cylindrical,
+    /// get the thermal resistance
+    ///
+    /// ln (d_o/d_i) * 1/(2 pi L lambda_insulation)
+    pub fn get_outer_shell_cylindrical_thermal_resistance(&self) -> 
+        Result<ThermalResistance, ThermalHydraulicsLibError> {
+
+
+            if !self.heat_exchanger_has_insulation {
+
+                // thermal resistance is not implemented if we don't have 
+                // insulation,
+                // probably want to have a nicer error for this in future
+                todo!();
+            }
+
+            let outer_shell_id = self.shell_side_id;
+            let outer_shell_od = self.shell_side_od;
+
+            let l = self.get_effective_length();
+
+            let lambda_insulation: ThermalConductivity;
+
+            let mut outer_shell_array_clone: SolidColumn = self.outer_shell
+                .clone()
+                .try_into()
+                .unwrap();
+
+            let outer_shell_array_temp: ThermodynamicTemperature = 
+                outer_shell_array_clone
+                .try_get_bulk_temperature()
+                .unwrap();
+
+            let outer_shell_array_material: SolidMaterial = 
+                outer_shell_array_clone.material_control_volume
+                .clone()
+                .try_into()
+                .unwrap();
+
+            lambda_insulation = 
+                outer_shell_array_material
+                .try_get_thermal_conductivity(
+                    outer_shell_array_temp
+                ).unwrap();
+
+
+            let log_numerator_term: f64 = 
+                (outer_shell_od/outer_shell_id)
+                .get::<ratio>()
+                .ln();
+
+            let denominator_term: ThermalConductance = 
+                2.0 * PI * lambda_insulation * l;
+
+
+            return Ok(log_numerator_term/denominator_term);
+
     }
 }
