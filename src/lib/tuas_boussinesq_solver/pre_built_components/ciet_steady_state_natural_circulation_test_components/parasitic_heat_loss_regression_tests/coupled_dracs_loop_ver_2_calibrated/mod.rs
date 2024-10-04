5c47b44b4f01844b353a1f474b289632572abbf1
guest@arch_gaminglaptop <theodore_ong@berkeley.edu>
2024-10-04 17:32:38 +0800

    tried changing nusselt calibration factor to 4.7 for sthe, not much diff

--- src/lib/tuas_boussinesq_solver/pre_built_components/ciet_steady_state_natural_circulation_test_components/parasitic_heat_loss_regression_tests/coupled_dracs_loop_ver_2_calibrated/mod.rs
+++ src/lib/tuas_boussinesq_solver/pre_built_components/ciet_steady_state_natural_circulation_test_components/parasitic_heat_loss_regression_tests/coupled_dracs_loop_ver_2_calibrated/mod.rs
@@ -17,14 +17,14 @@ pub fn regression_long_test_calibrated_ver2_set_c1(){
         experimental_pri_mass_flowrate_kg_per_s,
         simulated_expected_dracs_mass_flowrate_kg_per_s,
         simulated_expected_pri_mass_flowrate_kg_per_s) 
-        = (841.02, 40.0, 2.6860e-2, 2.0030e-2, 2.5765e-2, 2.1439e-2);
+        = (841.02, 40.0, 2.6860e-2, 2.0030e-2, 2.5814e-2, 2.1428e-2);
 
 
     let (shell_side_to_tubes_nusselt_number_correction_factor,
         insulation_thickness_regression_cm,
         shell_side_to_ambient_nusselt_correction_factor,
         dhx_heat_loss_to_ambient_watts_per_m2_kelvin) 
-        = (4.08,0.161,10.3,33.9);
+        = (4.7,0.161,10.3,33.9);
 
     let ( pri_loop_cold_leg_insulation_thickness_cm,
         pri_loop_hot_leg_insulation_thickness_cm,
@@ -76,14 +76,14 @@ pub fn regression_long_test_calibrated_ver2_set_c2(){
         experimental_pri_mass_flowrate_kg_per_s,
         simulated_expected_dracs_mass_flowrate_kg_per_s,
         simulated_expected_pri_mass_flowrate_kg_per_s) 
-        = (1158.69, 40.0, 3.0550e-2, 2.3670e-2, 3.0913e-2, 2.5365e-2);
+        = (1158.69, 40.0, 3.0550e-2, 2.3670e-2, 3.0965e-2, 2.5347e-2);
 
 
     let (shell_side_to_tubes_nusselt_number_correction_factor,
         insulation_thickness_regression_cm,
         shell_side_to_ambient_nusselt_correction_factor,
         dhx_heat_loss_to_ambient_watts_per_m2_kelvin) 
-        = (4.08,0.161,10.3,33.9);
+        = (4.7,0.161,10.3,33.9);
 
     let ( pri_loop_cold_leg_insulation_thickness_cm,
         pri_loop_hot_leg_insulation_thickness_cm,
@@ -135,14 +135,14 @@ pub fn regression_long_test_calibrated_ver2_set_c3(){
         experimental_pri_mass_flowrate_kg_per_s,
         simulated_expected_dracs_mass_flowrate_kg_per_s,
         simulated_expected_pri_mass_flowrate_kg_per_s) 
-        = (1409.22, 40.0, 3.3450e-2, 2.6350e-2, 3.4241e-2, 2.8001e-2);
+        = (1409.22, 40.0, 3.3450e-2, 2.6350e-2, 3.4296e-2, 2.7981e-2);
 
 
     let (shell_side_to_tubes_nusselt_number_correction_factor,
         insulation_thickness_regression_cm,
         shell_side_to_ambient_nusselt_correction_factor,
         dhx_heat_loss_to_ambient_watts_per_m2_kelvin) 
-        = (4.08,0.161,10.3,33.9);
+        = (4.7,0.161,10.3,33.9);
 
     let ( pri_loop_cold_leg_insulation_thickness_cm,
         pri_loop_hot_leg_insulation_thickness_cm,
@@ -194,14 +194,14 @@ pub fn regression_long_test_calibrated_ver2_set_c4(){
         experimental_pri_mass_flowrate_kg_per_s,
         simulated_expected_dracs_mass_flowrate_kg_per_s,
         simulated_expected_pri_mass_flowrate_kg_per_s) 
-        = (1736.11, 40.0, 3.6490e-2, 2.9490e-2, 3.7985e-2, 3.1011e-2);
+        = (1736.11, 40.0, 3.6490e-2, 2.9490e-2, 3.8046e-2, 3.0990e-2);
 
 
     let (shell_side_to_tubes_nusselt_number_correction_factor,
         insulation_thickness_regression_cm,
         shell_side_to_ambient_nusselt_correction_factor,
         dhx_heat_loss_to_ambient_watts_per_m2_kelvin) 
-        = (4.08,0.161,10.3,33.9);
+        = (4.7,0.161,10.3,33.9);
 
     let ( pri_loop_cold_leg_insulation_thickness_cm,
         pri_loop_hot_leg_insulation_thickness_cm,
@@ -254,14 +254,14 @@ pub fn regression_long_test_calibrated_ver2_set_c5(){
         experimental_pri_mass_flowrate_kg_per_s,
         simulated_expected_dracs_mass_flowrate_kg_per_s,
         simulated_expected_pri_mass_flowrate_kg_per_s) 
-        = (2026.29, 40.0, 3.8690e-2, 3.1900e-2, 4.0905e-2, 3.3367e-2);
+        = (2026.29, 40.0, 3.8690e-2, 3.1900e-2, 4.0970e-2, 3.3346e-2);
 
 
     let (shell_side_to_tubes_nusselt_number_correction_factor,
         insulation_thickness_regression_cm,
         shell_side_to_ambient_nusselt_correction_factor,
         dhx_heat_loss_to_ambient_watts_per_m2_kelvin) 
-        = (4.08,0.161,10.3,33.9);
+        = (4.7,0.161,10.3,33.9);
 
     let ( pri_loop_cold_leg_insulation_thickness_cm,
         pri_loop_hot_leg_insulation_thickness_cm,
@@ -313,14 +313,14 @@ pub fn regression_long_test_calibrated_ver2_set_c6(){
         experimental_pri_mass_flowrate_kg_per_s,
         simulated_expected_dracs_mass_flowrate_kg_per_s,
         simulated_expected_pri_mass_flowrate_kg_per_s) 
-        = (2288.83, 40.0, 4.1150e-2, 3.4120e-2, 4.3300e-2, 3.5293e-2);
+        = (2288.83, 40.0, 4.1150e-2, 3.4120e-2, 4.3369e-2, 3.5275e-2);
 
 
     let (shell_side_to_tubes_nusselt_number_correction_factor,
         insulation_thickness_regression_cm,
         shell_side_to_ambient_nusselt_correction_factor,
         dhx_heat_loss_to_ambient_watts_per_m2_kelvin) 
-        = (4.08,0.161,10.3,33.9);
+        = (4.7,0.161,10.3,33.9);
 
     let ( pri_loop_cold_leg_insulation_thickness_cm,
         pri_loop_hot_leg_insulation_thickness_cm,
@@ -373,14 +373,14 @@ pub fn regression_long_test_calibrated_ver2_set_c7(){
         experimental_pri_mass_flowrate_kg_per_s,
         simulated_expected_dracs_mass_flowrate_kg_per_s,
         simulated_expected_pri_mass_flowrate_kg_per_s) 
-        = (2508.71, 40.0, 4.3120e-2, 3.5620e-2, 4.5160e-2, 3.6781e-2);
+        = (2508.71, 40.0, 4.3120e-2, 3.5620e-2, 4.5232e-2, 3.6764e-2);
 
 
     let (shell_side_to_tubes_nusselt_number_correction_factor,
         insulation_thickness_regression_cm,
         shell_side_to_ambient_nusselt_correction_factor,
         dhx_heat_loss_to_ambient_watts_per_m2_kelvin) 
-        = (4.08,0.161,10.3,33.9);
+        = (4.7,0.161,10.3,33.9);
 
     let ( pri_loop_cold_leg_insulation_thickness_cm,
         pri_loop_hot_leg_insulation_thickness_cm,
@@ -434,14 +434,14 @@ pub fn regression_long_test_calibrated_ver2_set_c8(){
         experimental_pri_mass_flowrate_kg_per_s,
         simulated_expected_dracs_mass_flowrate_kg_per_s,
         simulated_expected_pri_mass_flowrate_kg_per_s) 
-        = (2685.83, 40.0, 4.5090e-2, 3.5930e-2, 4.6577e-2, 3.7907e-2);
+        = (2685.83, 40.0, 4.5090e-2, 3.5930e-2, 4.6650e-2, 3.7891e-2);
 
 
     let (shell_side_to_tubes_nusselt_number_correction_factor,
         insulation_thickness_regression_cm,
         shell_side_to_ambient_nusselt_correction_factor,
         dhx_heat_loss_to_ambient_watts_per_m2_kelvin) 
-        = (4.08,0.161,10.3,33.9);
+        = (4.7,0.161,10.3,33.9);
 
     let ( pri_loop_cold_leg_insulation_thickness_cm,
         pri_loop_hot_leg_insulation_thickness_cm,
@@ -495,14 +495,14 @@ pub fn regression_long_test_calibrated_ver2_set_c9(){
         experimental_pri_mass_flowrate_kg_per_s,
         simulated_expected_dracs_mass_flowrate_kg_per_s,
         simulated_expected_pri_mass_flowrate_kg_per_s) 
-        = (2764.53, 40.0, 4.6990e-2, 3.5470e-2, 4.7185e-2, 3.8387e-2);
+        = (2764.53, 40.0, 4.6990e-2, 3.5470e-2, 4.7259e-2, 3.8373e-2);
 
 
     let (shell_side_to_tubes_nusselt_number_correction_factor,
         insulation_thickness_regression_cm,
         shell_side_to_ambient_nusselt_correction_factor,
         dhx_heat_loss_to_ambient_watts_per_m2_kelvin) 
-        = (4.08,0.161,10.3,33.9);
+        = (4.7,0.161,10.3,33.9);
 
     let ( pri_loop_cold_leg_insulation_thickness_cm,
         pri_loop_hot_leg_insulation_thickness_cm,
@@ -556,7 +556,7 @@ pub fn regression_long_test_calibrated_ver2_set_c9(){
 ///
 /// for the DHX STHE,
 ///
-/// shell side to tubes nusselt correction factor is 4.08
+/// shell side to tubes nusselt correction factor is 4.7
 /// insulation thickness is 0.161 cm 
 /// shell side to ambient correction factor is 10.3 
 /// heat loss to ambient is 33.9 W/(m^2 K)
